706b8d5d1f742e021ee9320980f0b5778fe335a8
SvetlanaNesmiyan <Svitlana.Nesmiian@infiz.khpi.edu.ua>
2025-10-30 19:49:22 +0200

    lab6

--- public/lab6/config.php
+++ public/lab6/config.php
@@ -1,16 +1,16 @@
 <?php
-
 session_start();
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'testuser');
-define('DB_PASS', 'password123');
+define('DB_HOST', '127.0.0.1'); // Змініть на 127.0.0.1
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'users_db');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo "Успішне підключення до БД!";
 } catch(PDOException $e) {
     die("Помилка підключення до бази даних: " . $e->getMessage());
 }
